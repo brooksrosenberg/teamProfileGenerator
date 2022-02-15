c548b07b4e1af44b2b9bc988f9c694961e20b2b6
Brooks Rosenberg <bjtr@bu.edu>
2022-02-14 20:05:59 -0500

    added v1 index and fixed .toBe error in v1 Employee.test

--- Develop/tests/Employee.test.js
+++ Develop/tests/Employee.test.js
@@ -1,26 +1,25 @@
-const { expect } = require("@jest/globals");
-const { it } = require("jest-circus");
+
 const Employee = require("./Develop/lib/Employee");
 
 describe("Employee", () => {
     it("creates Employee input", () => {
         const newEmployee = new Employee();
-        expect(typeof(newEmployee))toBe("object")
+        expect(typeof(newEmployee)).toBe("object")
     });
 
 
     it("generates name from constructor", () => {
         const name = getName();
-        expect(newEmployee.name)toBe(name)
+        expect(newEmployee.name).toBe(name);
     });
 
     it("generates id from constructor", () => {
         const id = getId();
-        expect(newEmployee.id)toBe(id)
+        expect(newEmployee.id).toBe(id);
     });
     it("generates Email from constructor", () => {
         const email = getEmail();
-        expect(newEmployee.email)toBe(email)
+        expect(newEmployee.email).toBe(email);
     });
 
   });
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const Employee = require("./Develop/lib/Employee");
+
+const employee = new Employee();
+ 
+employee.getName();
+employee.getId();
+employee.getEmail();
+employee.getRole();
